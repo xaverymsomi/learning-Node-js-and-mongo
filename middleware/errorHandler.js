54fb62c5f38589cf24ab824eb55e856f68a2b019
Vicent Msomi <msomivicent@gmail.com>
2025-04-10 21:37:11 +0300

    initial commit

---
+++ middleware/errorHandler.js
@@ -0,0 +1,26 @@
+const { constants } = require("../constants")
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+            res.json({ title: "Validation failed", message: err.message, stackTrace: err.stack });
+            break;
+        case constants.NOT_FOUND:
+            res.json({ title: "Not Found", message: err.message, stackTrace: err.stack });
+            break;
+        case constants.FORBIDDEN:
+            res.json({ title: "Not Found", message: err.message, stackTrace: err.stack });
+            break;
+        case constants.UNAUTHORIZED:
+            res.json({ title: "Not Found", message: err.message, stackTrace: err.stack });
+            break;
+        default:
+            break;
+    }
+     
+
+     
+};
+
+module.exports = errorHandler;
---
+++ server.js
@@ -0,0 +1,18 @@
+// basic server creation
+
+const express  = require('express');
+const errorHandler = require('./middleware/errorHandler');
+const dotenv  = require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// Middleware to parse JSON request body
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(` Server is current runing on port number ${port}`);
+})
